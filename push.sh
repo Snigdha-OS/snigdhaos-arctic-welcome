83871bc70c20a7ac19273bfbbee7d56d73af2da3
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-05-02 21:39:23 +0530

    WIP: re writing push script"

--- push.sh
+++ push.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-# Set the repository URL and branch
-REPO_URL="https://github.com/Snigdha-OS/snigdhaos-welcome.git"
-BRANCH="master"  # or "main" depending on your repository's default branch
-
-# Commit message
-MESSAGE="⏳ @eshanized updated the repository!!!"
-
-# Add all files, commit, and push changes
-git add .
-git commit -m "$MESSAGE"
-git push origin $BRANCH
+#
